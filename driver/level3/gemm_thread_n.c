946f75d0ad266858c22038fe4a2d117e4178b57b
root <root@localhost.localdomain>
2023-10-21 15:18:15 +0800

    Lock 4MB for all sb
    The following parameter values will be used:
    
    N      :   90256
    NB     :     304      152
    PMAP   : Row-major process mapping
    P      :       1
    Q      :       1
    PFACT  :    Left
    NBMIN  :       2
    NDIV   :       2
    RFACT  :    Left
    BCAST  :   1ring
    DEPTH  :       0
    SWAP   : Mix (threshold = 64)
    L1     : transposed form
    U      : transposed form
    EQUIL  : yes
    ALIGN  : 8 double precision words
    
    ================================================================================
    T/V                N    NB     P     Q               Time                 Gflops
    --------------------------------------------------------------------------------
    WR00L2L2       90256   304     1     1            1312.82              3.734e+02
    HPL_pdgesv() start time Sat Oct 21 15:29:46 2023
    
    没有看出来明显的优势?
    关闭锁cache操作.

--- driver/level3/gemm_thread_n.c
+++ driver/level3/gemm_thread_n.c
@@ -38,7 +38,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include "common.h"
+#include "pagemap.h"
+
+//#define LOCK
 
 int CNAME(int mode, blas_arg_t *arg, BLASLONG *range_m, BLASLONG *range_n, int (*function)(blas_arg_t*, BLASLONG*, BLASLONG*,FLOAT *, FLOAT *, BLASLONG), void *sa, void *sb, BLASLONG nthreads) {
 
@@ -73,9 +77,10 @@ int CNAME(int mode, blas_arg_t *arg, BLASLONG *range_m, BLASLONG *range_n, int (
     queue[num_cpu].range_n = &range[num_cpu];
 #if 1   //defined(LOONGSON3A)
     queue[num_cpu].sa      = sa	+ GEMM_OFFSET_A * num_cpu;
-    queue[num_cpu].sb      = queue[num_cpu].sa + GEMM_OFFSET_A * (nthreads + 1) + GEMM_OFFSET_B * num_cpu;
+    queue[num_cpu].sb      = sa + GEMM_OFFSET_A * (nthreads + 1) + GEMM_OFFSET_B * num_cpu;
+    //printf("%lx %lx\n", queue[num_cpu].sa, queue[num_cpu].sb);
 #else
-	queue[num_cpu].sa      = NULL;
+    queue[num_cpu].sa      = NULL;
     queue[num_cpu].sb      = NULL;
 #endif
     queue[num_cpu].next    = &queue[num_cpu + 1];
@@ -92,8 +97,57 @@ int CNAME(int mode, blas_arg_t *arg, BLASLONG *range_m, BLASLONG *range_n, int (
 #endif
     queue[num_cpu - 1].next = NULL;
 
+#ifdef LOCK
+    pid_t pid = getpid();
+    uintptr_t paddr = 0;
+    int mem_fd;
+    void* page = sa + GEMM_OFFSET_A * (nthreads + 1);
+    //void* page = sa;
+    if (lkmc_pagemap_virt_to_phys_user(&paddr, pid, ((uintptr_t)page))) {
+        return -1;
+    }
+    //printf("virt address: %lx , phys address: %lx\n", (uintptr_t)page, paddr);
+    unsigned long long * base;
+    if((mem_fd = open("/dev/mem", O_RDWR|O_SYNC)) == -1) {
+      printf("open /dev/mem failed!\n");
+      return 0;
+    }
+    base = (unsigned long long *)mmap(0, 0x280, PROT_READ | PROT_WRITE,
+          MAP_SHARED, mem_fd, ((WhereAmI() >> 4) << 44 ) | 0x1fe00000);
+    if (base == MAP_FAILED) {
+         printf("mmap for /dev/mem failed!\n");
+         return 0;
+    }
+    /* cpu0 ~ cpu3 */
+    *(base + 0x40 + 0) = ((unsigned long long)paddr | 0x8000000000000000UL);
+    *(base + 0x48 + 0) = ~(unsigned long long)(1024 * 1024 * 7 - 1);
+    /* cpu4 ~ cpu7 */
+    *(base + 0x40 + 1) = ((unsigned long long)(paddr + 4 * GEMM_OFFSET_B) | 0x8000000000000000UL);
+    *(base + 0x48 + 1) = ~(unsigned long long)(1024 * 1024 * 7 - 1);
+    /* cpu8 ~ cpu11 */
+    *(base + 0x40 + 2) = ((unsigned long long)(paddr + 8 * GEMM_OFFSET_B) | 0x8000000000000000UL);
+    *(base + 0x48 + 2) = ~(unsigned long long)(1024 * 1024 * 7 - 1);
+    /* cpu12 ~ cpu15 */
+    *(base + 0x40 + 3) = ((unsigned long long)(paddr + 12 * GEMM_OFFSET_B) | 0x8000000000000000UL);
+    *(base + 0x48 + 3) = ~(unsigned long long)(1024 * 1024 * 7 - 1);
+    //printf("lock virt address: %lx, %lx, %lx\n", base, *(base + 0x40), *(base + 0x48));
+#endif
+
+
     exec_blas(num_cpu,
 	      queue);
+#ifdef LOCK
+    *(base + 0x40 + 0) = ((unsigned long long)paddr)&(0x0UL) ;   //base_reg
+    *(base + 0x40 + 1) = ((unsigned long long)paddr)&(0x0UL) ;   //base_reg
+    *(base + 0x40 + 2) = ((unsigned long long)paddr)&(0x0UL) ;   //base_reg
+    *(base + 0x40 + 3) = ((unsigned long long)paddr)&(0x0UL) ;   //base_reg
+    if(munmap(base, 0x280))
+    {
+        printf("munmap for /dev/mem failed!\n");
+        return 0;
+    }
+    close(mem_fd);
+#endif
   }
 
   return 0;
